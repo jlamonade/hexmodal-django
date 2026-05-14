f6da6b5cd73e531bcdf4001d6ef0d6a9f1377309
Jason Lam <jason.lam@intersection.com>
2026-05-13 21:40:11 -0400

    implement admin

--- hexmodal/admin.py
+++ hexmodal/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from hexmodal.models import Device, Payload
+
+
 # Register your models here.
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("name", "status", "created_at")
+    search_fields = ("name", "description")
+    list_filter = ("status", "created_at")
+
+
+class PayloadAdmin(admin.ModelAdmin):
+    list_display = ("fCnt", "deviceEUI", "created_at")
+    search_fields = ("deviceEUI__name",)
+    list_filter = ("created_at",)
+
+
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Payload, PayloadAdmin)
