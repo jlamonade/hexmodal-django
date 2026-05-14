b412e69714d927575886b36a0f7c6b9334f389a4
Jason Lam <jason.lam@intersection.com>
2026-05-13 21:33:38 -0400

    implemented model views

--- hexmodal/views.py
+++ hexmodal/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Device, Payload
+from .serializers import DeviceSerializer, PayloadSerializer
+
 
 # Create your views here.
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+
+class PayloadViewSet(viewsets.ModelViewSet):
+    queryset = Payload.objects.all()
+    serializer_class = PayloadSerializer
