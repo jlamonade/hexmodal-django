9f4220e8db2655699646c147921609bf7555da89
Jason Lam <jason.lam@intersection.com>
2026-05-13 22:44:47 -0400

    fix models using iot and lora concepts

--- hexmodal/models.py
+++ hexmodal/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 DEVICE_STATUS_CHOICES = [
     ("passing", "Passing"),
     ("failing", "Failing"),
@@ -8,7 +7,7 @@ DEVICE_STATUS_CHOICES = [
 
 
 class Device(models.Model):
-    id = models.AutoField(primary_key=True, unique=True, editable=False) # TODO use UUID later, maybe
+    devEUI = models.CharField(max_length=16, primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     status = models.CharField(max_length=20, choices=DEVICE_STATUS_CHOICES, default="passing")
@@ -19,12 +18,18 @@ class Device(models.Model):
 
 
 class Payload(models.Model):
-    fCnt = models.IntegerField(primary_key=True, unique=True, editable=False)  # message counter, unique and auto-incrementing
-    deviceEUI = models.ForeignKey(Device, on_delete=models.CASCADE)  # foreign key to Device model
-    data = models.CharField(max_length=512)  # loRaWAN payload data as a hex string, max payload size is 242 bytes, 484 hex characters
-    rxInfo = models.JSONField()  # JSON field to store rxInfo array
-    txInfo = models.JSONField()  # JSON field to store txInfo object
+    fCnt = models.IntegerField()
+    devEUI = models.ForeignKey(Device, on_delete=models.CASCADE)
+    data = models.CharField(max_length=512)
+    rxInfo = models.JSONField()
+    txInfo = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["devEUI", "fCnt"], name="unique_payload_per_device"),
+        ]
+
+
     def __str__(self):
-        return f"Payload for {self.deviceEUI} at {self.created_at}"
+        return f"Payload for {self.devEUI} at {self.created_at}"
