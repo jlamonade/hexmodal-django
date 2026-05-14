c0aa22eaf9be7d4e912a12f505a6d0548f49100a
Jason Lam <jason.lam@intersection.com>
2026-05-13 21:39:47 -0400

    implement pk field for device

--- hexmodal/models.py
+++ hexmodal/models.py
@@ -8,6 +8,7 @@ DEVICE_STATUS_CHOICES = [
 
 
 class Device(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     status = models.CharField(max_length=20, choices=DEVICE_STATUS_CHOICES, default="passing")
